57af3860ff9a30f1b49d7fbc2ee42b22b48625a2
gustavo.schwan <gustavo.schwan@192.168.56.1>
2019-08-08 19:45:00 -0300

    Comit Inicial

---
+++ src/univates/ClassePrincipal.java
@@ -0,0 +1,35 @@
+package univates;
+
+/**
+ *
+ * @author gustavo.schwan
+ */
+public class ClassePrincipal {
+    public static void main(String[] args){
+        
+    soma(2,3); //imprima o resultado na tela
+    subtrai(3,3); //mesma coisa
+     
+    //teste condiocional (verdade ou falso)
+    String var1 = "Juca";
+    String var2 = "Bala";
+    
+    //equalsIgnoreCase = comparatexto ignorando o CaseSensitive
+    if(var1.equals(var2) && 1 ==1){
+        System.out.println("IF");
+    } else{
+        System.out.println("ELSE");
+    }
+     
+    }
+    public static void soma(int var1, int var2){
+        int result = var1 + var2;
+        System.out.println("Resultado = " + result);
+    }
+    public static void subtrai(int num1, int  num2){
+        int result = num1 - num2;
+        System.out.println(result + "Resultado = ");
+        
+    }
+}
+    
